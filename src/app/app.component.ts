d92b2111ac6781b217136d4ffeb0b3ad43d500a7
iqbalthex <73935864+iqbalthex@users.noreply.github.com>
2022-05-25 17:37:02 +0700

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'windrose';
 
-	currYear: number = 2020;
+	currYear: number = 2022;
 	currMonth: string = "Mei";
 	years: number[] = [2015,2016,2017,2018,2019,2020,2021,2022];
 	months: string[] = ['Januari','Februari','Maret','April','Mei','Juni',
@@ -20,26 +20,37 @@ export class AppComponent {
 	daysOfMonth: {[key: string]: any} = { "Januari": 31, "Maret": 31,
 		"April": 30, "Mei": 31, "Juni": 30, "Juli": 31, "Agustus": 31,
 		"September": 30, "Oktober": 31, "November": 30, "Desember": 31,
-		"Februari": (year: number) => year % 4 == 0 ? 29 : 28
+		"Februari": (year: number = this.currYear) => year % 4 == 0 ? 29 : 28
 	};
-	daysInMonth: {[key: string]: number[]} = {
-		"2020-Februari": [1,2,3],
-		"2021-Februari": [1,2],
-		"2020-Maret": [1,2,3,4],
-		"2021-Maret": [1,2,3,4]
-	};
-	for (i=0;i<) // loop daysInMonth
+	setDay = (month: string,arr: boolean = true): number[] => {
+		let tgl: number[] = [];
+		let days: number;
+		days = month == "Februari" ? this.daysOfMonth[month]() : this.daysOfMonth[month];
+		for (let i=0;i<days;i++){
+			tgl.push(arr ? i+1 : 0);
+		}
+		return tgl;
+	}
 
 	setCurrYear = (year: number): void => {
 		this.currYear = year;
-		this.tgl = this.daysInMonth[this.currYear+'-'+this.currMonth];
+		this.tgl = this.setDay(this.currMonth);
 	}
 
 	setCurrMonth = (month: string): void => {
 		this.currMonth = month;
-		this.tgl = this.daysInMonth[this.currYear+'-'+this.currMonth];
+		this.tgl = this.setDay(this.currMonth);
 	}
 
-	tgl: number[] = this.daysInMonth[this.currYear+'-'+this.currMonth];
+	tgl: number[] = this.setDay(this.currMonth);
+
+	velData: {[key:string]: number[]} = {};
+	tblLoad = () => {
+		for (let i=0;i<this.years.length;i++){
+			for (let j=0;j<this.months.length;j++){
+				this.velData[this.years[i]+'-'+this.months[j]] = [1,2,3];
+			}			
+		}
+	}
 
 }
